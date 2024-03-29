10ff020d23559f1ab18cc68219beb119392b6676
mücahid uygun <uygun3091@hotmail.com>
2024-03-29 03:38:07 +0300

    İnitial Project

---
+++ src/app/models/responses/bootcamp/getlist-bootcamp-response.ts
@@ -0,0 +1,8 @@
+export interface GetlistBootcampResponse {
+    name:string;
+    instructorFirstName:string;
+    instructorLastName:string;
+    bootcampStateName:number;
+    startDate:Date;
+    endDate:Date;
+}
---
+++ src/app/pages/homepage/homepage.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnChanges, OnDestroy, OnInit } from '@angular/core';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { GetlistBootcampResponse } from '../../models/responses/bootcamp/getlist-bootcamp-response';
+import { GetlistBootcampstateResponse } from '../../models/responses/bootcampstate/getlist-bootcampstate-response';
+import { DataResult } from '../../models/DataResult';
+
+@Component({
+  selector: 'app-homepage',
+  standalone: true,
+  imports: [RouterOutlet,RouterModule,CommonModule,HttpClientModule,NavbarComponent],
+  templateUrl: './homepage.component.html',
+  styleUrl: './homepage.component.scss'
+})
+export class HomepageComponent implements OnInit{
+
+  bootcampList:GetlistBootcampResponse[] = [];
+  bootcampState:GetlistBootcampstateResponse[] = [];
+  
+  constructor(private httpClient:HttpClient){}
+
+  ngOnInit(): void {
+    this.getListModels();
+  }
+ getListModels(){
+    this.httpClient.get<DataResult<GetlistBootcampResponse[]>>("http://localhost:5278/api/Bootcamps")
+    .subscribe({
+      next:(response:DataResult<GetlistBootcampResponse[]>)=>{
+        console.log("Cevap geldi :",response);
+        this.bootcampList=response.data;
+      },
+      error:(error)=>{console.log("cevap hatalı :",error)},
+      complete:()=>{console.log("istek sonlandı")}
+    })
+  }
+  }
