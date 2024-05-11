2fe210924177a60d82402ced717fcf067fc1464e
ZafranBlue <enriq@ZAFRANBLUE>
2024-05-11 15:36:17 +0300

    Test

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,19 +4,14 @@ import { LoginComponent } from './pages/login/login.component';
 import { NavbarComponent } from '@coreui/angular';
 import { AuthService } from './features/services/concretes/auth.service';
 import { AdminpanelComponent } from './pages/adminpanel/adminpanel.component';
-<<<<<<< HEAD
+
 import { ProfileComponent } from './pages/profile/profile.component';
-=======
->>>>>>> 6b1aad005f0297ed37ddc8f7d04e2f6d7fe6fde6
 
 export const routes: Routes = [
     {path:'',redirectTo:'homepage',pathMatch:'full'},
     {path:'homepage',component:HomepageComponent},
     {path:'login' ,component:LoginComponent},
     {path:'adminpanel' ,component:AdminpanelComponent},
-<<<<<<< HEAD
     {path:'profil' ,component:ProfileComponent},
-=======
->>>>>>> 6b1aad005f0297ed37ddc8f7d04e2f6d7fe6fde6
     {path:'' ,component:NavbarComponent, canActivate:[AuthService]}
 ];
